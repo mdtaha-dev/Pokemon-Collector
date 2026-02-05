358c943dce21db3f0d445f0d9e35318709ddf103
mdtaha-dev <mohammedtaha.hashmi@gmail.com>
2026-02-05 21:33:52 +0530

    Initial commit

---
+++ store.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def hello_world():
+    return render_template('home.html')
+
+@app.route('/about/<username>')
+def about_page(username):
+    return f"<h1>This is the about page of {username}</h1>"
+
