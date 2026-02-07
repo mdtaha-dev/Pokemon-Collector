8dded581de31d8b5d7a3424368454aff51b15737
mdtaha-dev <mohammedtaha.hashmi@gmail.com>
2026-02-07 13:24:40 +0530

    SQLite implementation

--- store.py
+++ store.py
@@ -1,24 +1,33 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route("/")
-@app.route("/home")
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False, unique=True)
+    price = db.Column(db.Integer, nullable=False)
+    barcode = db.Column(db.String(12), nullable=False, unique=True)
+    description = db.Column(db.String(1024), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f"<Item {self.name}>"
+
+@app.route('/')
+@app.route('/home')
 def home_page():
-    return render_template("home.html")
+    return render_template('home.html')
 
-@app.route("/about/<username>")
-def about_page(username):
-    return f"<h1>This is the about page of {username}</h1>"
-
-@app.route("/market")
+@app.route('/market')
 def market_page():
-    items = [
-        {'id': 1, 'name': 'Pikachu', 'barcode': '893212299897', 'price': 500},
-        {'id': 2, 'name': 'Charmander', 'barcode': '123985473165', 'price': 900},
-        {'id': 3, 'name': 'Lugia', 'barcode': '231985128446', 'price': 2000},
-        {'id': 4, 'name': 'Mewtwo', 'barcode': '841965028223', 'price': 1200}
-    ]
-    return render_template("market.html", items=items)
+    items = Item.query.all()
+    return render_template('market.html', items=items)
 
-
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
