9bc67166044a87cad7602d39d4ea75b6a08dff55
mdtaha-dev <mohammedtaha.hashmi@gmail.com>
2026-02-09 20:40:28 +0530

    Fixed Database

--- market/models.py
+++ market/models.py
@@ -1,5 +1,16 @@
 from market import db
 
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), nullable=False, unique=True)
+    email_address = db.Column(db.String(50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(60), nullable=False)
+    budget = db.Column(db.Integer, nullable=False, default=1000)
+
+    items = db.relationship('Item', backref='owned_user', lazy=True)
+
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -7,6 +18,10 @@ class Item(db.Model):
     price = db.Column(db.Integer, nullable=False)
     barcode = db.Column(db.String(12), nullable=False, unique=True)
     description = db.Column(db.String(1024), nullable=False)
+    owner = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id')
+    )
 
     def __repr__(self):
         return f"<Item {self.name}>"
