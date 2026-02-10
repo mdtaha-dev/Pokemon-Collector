a71daf24b810e8a20e8310a633e7960ab4691bbd
mdtaha-dev <mohammedtaha.hashmi@gmail.com>
2026-02-10 20:21:17 +0530

    Finalized Login Page

--- market/__init__.py
+++ market/__init__.py
@@ -2,12 +2,14 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__, instance_relative_config=True)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     'sqlite:///' + os.path.join(app.instance_path, 'market.db')
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'd935d2eea177cc66e70f6c72'
 
 db = SQLAlchemy(app)
 
